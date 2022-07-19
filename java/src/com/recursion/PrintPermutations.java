2b0c258f54015ea8423e2e3740c420b9a95c4a2e
saurabhnakade <saurabh.nakade23@gmail.com>
2022-07-19 16:46:49 +0530

    day26

---
+++ java/src/com/recursion/PrintPermutations.java
@@ -0,0 +1,26 @@
+package com.recursion;
+
+import java.util.Scanner;
+
+public class PrintPermutations {
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        String x=scn.next();
+
+        printPermutations(x,"");
+
+    }
+
+    public static void printPermutations(String str, String asf) {
+        if(str.length()==0){
+            System.out.println(asf);
+            return;
+        }
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            String ros=str.substring(0,i)+str.substring(i+1);
+            printPermutations(ros,asf+ch);
+        }
+    }
+}
